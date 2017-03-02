4ed9c65a4537f334a94f65703ed374c4cf49f2c6
MiszelHub <nolanb.o.s@gmail.com>
2017-03-02 19:14:30 +0100

    zmiana struktury

---
+++ src/main/java/TesPackage/App.java
@@ -0,0 +1,13 @@
+package TesPackage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/TesPackage/AppTest.java
@@ -0,0 +1,39 @@
+package TesPackage;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testAssertJ()
+    {
+        assertThat(true);
+    }
+}
